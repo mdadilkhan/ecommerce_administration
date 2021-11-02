624c2fcff9ea015b0ad6ebcad0a7e31ac9edcf1b
Md Adil Khan <mdadilakhtar8@gmail.com>
2021-11-02 23:10:31 +0530

    added home signin and signup component

---
+++ src/containers/Home/index.js
@@ -0,0 +1,20 @@
+import { Container } from "react-bootstrap"
+import React from 'react'
+import Layout from '../../components/Layout'
+
+function Home() {
+    return (
+        <>
+         <Layout>
+         <Container style={{margin:'5rem',background:'#fff',textAlign:'center'}} className="text_area">
+            <h1> welcome to admin dashboard</h1>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English</p>
+         </Container>
+         </Layout>
+       
+       
+       </>
+    )
+}
+
+export default Home
